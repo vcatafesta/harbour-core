2d7d66fa62d33725aa07d1bd57fa1159a7c70baf
Vilmar Catafesta <vcatafesta@gmail.com>
2020-05-29 23:58:42 -0400

    Update dattime3.c

--- contrib/hbct/dattime3.c
+++ contrib/hbct/dattime3.c
@@ -154,7 +154,8 @@ HB_FUNC( SETTIME )
       lNewTime = iTime[ 0 ] * 3600 + iTime[ 1 ] * 60 + iTime[ 2 ];
       tm       = time( NULL );
       tm      += lNewTime - ( tm % 86400 );
-      fResult  = stime( &tm ) == 0;
+      //fResult  = stime( &tm ) == 0;
+      fResult  = clock_settime( CLOCK_REALTIME, &tm ) == 0;
 #elif defined( HB_OS_DOS )
       union REGS regs;
       regs.h.ah = 45;
@@ -197,7 +198,8 @@ HB_FUNC( SETDATE )
          lNewDate = lDate - hb_dateEncode( 1970, 1, 1 );
          tm       = time( NULL );
          tm       = lNewDate * 86400 + ( tm % 86400 );
-         fResult  = stime( &tm ) == 0;
+         //fResult  = stime( &tm ) == 0;
+         fResult  = clock_settime( CLOCK_REALTIME, &tm ) == 0;
 #elif defined( HB_OS_DOS )
          union REGS regs;
          regs.h.ah        = 43;
